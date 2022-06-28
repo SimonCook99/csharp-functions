2106fbe45305aafacc5a8a28c1b274ab3f57e91d
Simone Mauro <simon.mauro@virgilio.it>
2022-06-28 15:13:17 +0200

    Esercizio con bonus completato

--- Program.cs
+++ Program.cs
@@ -1,17 +1,43 @@
 ﻿
 
-int[] array = { 1, 2, 3 };
-StampaArray(array);
+//SEZIONE ARRAY PREDEFINITO
+//int[] array = { 2, 6, 7, 5, 3, 9 };
+//StampaArray(array);
 
-Console.WriteLine("\nIl quadrato è: " + Quadrato(9));
+//Console.WriteLine("\nIl quadrato è: " + Quadrato(9));
 
-int[] arrayElevato = ElevaArrayAlQuadrato(array);
+//int[] arrayElevato = ElevaArrayAlQuadrato(array);
 
-for (int i = 0; i < arrayElevato.Length; i++) {
-    Console.WriteLine(arrayElevato[i]);
+//StampaArray(array);
+//StampaArray(arrayElevato);
+
+
+//Console.WriteLine("La somma è: " + SommaElementiArray(array));
+//Console.WriteLine("La somma è: " + SommaElementiArray(arrayElevato));
+
+
+//SEZIONE ARRAY DEFINITO DALL'UTENTE (inclusa la lunghezza)
+Console.WriteLine("Quanti numeri vuoi inserire?");
+int lunghezzaArray = int.Parse(Console.ReadLine());
+
+int[] arrayUtente = new int[lunghezzaArray];
+
+for (int i = 0; i < arrayUtente.Length; i++){
+    Console.WriteLine($"Inserisci l'elemento {i + 1}");
+    arrayUtente[i] = int.Parse(Console.ReadLine());
 }
 
-Console.WriteLine("La somma è: " + SommaElementiArray(array));
+StampaArray(arrayUtente);
+
+int[] arrayElevato = ElevaArrayAlQuadrato(arrayUtente);
+
+StampaArray(arrayElevato);
+StampaArray(arrayUtente);
+
+Console.WriteLine("La somma degli elementi è: " + SommaElementiArray(arrayUtente));
+Console.WriteLine("La somma degli elementi al quadrato è: " + SommaElementiArray(arrayElevato));
+
+
 
 
 //funzione 1, stampa array
@@ -21,8 +47,7 @@ void StampaArray(int[] array){
             Console.Write($"[{array[i]},");
         }else if (i == array.Length - 1){
             Console.Write($"{array[i]}]");
-        }
-        else{
+        }else{
             Console.Write($"{array[i]},");
         }
     }
